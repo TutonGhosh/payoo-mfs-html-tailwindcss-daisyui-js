2fbb446c067429dcf183070d1774cdfa196fd7d1
TutonGhosh <404ghosh@gmail.com>
2026-04-05 12:54:24 +0600

    js added to add money section

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function logIn(event)
     }
     else
     {
-        alert("Please enter a valid number and pin!");
+        alert("Invalid Number and Pin!");
     }
 }
 
@@ -28,4 +28,37 @@ function logOut()
 function addMoney()
 {
     window.location.href = "./addmoney.html"
+}
+// Add Money Function
+function addMoneyBtn()
+{
+    const mainBalance = document.getElementById('main-balance');
+    const inputAccNumber = document.getElementById('input-acc-number');
+    const inputAmount = document.getElementById('input-amount');
+    const inputPin = document.getElementById('input-pin');
+
+    let balance = parseInt(mainBalance.innerText.replace('$', ''));
+    const accNumber = parseInt(inputAccNumber.value);
+    const amount = parseInt(inputAmount.value);
+    const pin = parseInt(inputPin.value);
+
+
+    if((!isNaN(accNumber) && inputAccNumber.value.length === 11) && (!isNaN(pin) && inputPin.value.length === 4))
+    {
+        balance += amount;
+        mainBalance.innerText = '$' + balance;
+        localStorage.setItem('mainBalance', balance);
+        window.location.href = './payment_successful.html';
+    }
+    else
+    {
+        alert("Invalid Input");
+    }
+}
+
+// Back to Home
+function backToHome(mainBalance)
+{
+
+    window.location.href = "./home.html"
 }
